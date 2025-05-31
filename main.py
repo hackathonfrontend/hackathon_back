46ee524e6dc8636140ec20e1f83976acfa2ad02b
Simioniuc Ionut-Mihai <simioniucionut@gmail.com>
2025-05-31 19:10:17 +0300

    up

--- main.py
+++ main.py
@@ -2,11 +2,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from config.database import create_tables
-<<<<<<< HEAD
-from app.routers.user_router import router as user_router
-=======
-from app.routers import user_routers # Or your specific router module
->>>>>>> main
+from app.routers import user_router # Or your specific router module
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,9 +13,4 @@ async def lifespan(app: FastAPI):
     # For example: await close_database_connections()
 
 app = FastAPI(lifespan=lifespan)
-<<<<<<< HEAD
-app.include_router(user_router)
-=======
-
-app.include_router(user_routers.router)
->>>>>>> main
+app.include_router(user_router.router)
